ccad78713ebc5608cfbb42c60fd6d7a27e279dbb
christios <christian-cayralat@hotmail.com>
2020-10-08 15:58:57 +0200

    Include necessary options for command line and several nortable updates

--- text-summarization/lsa.py
+++ text-summarization/lsa.py
@@ -1,63 +1,81 @@
-import sys
 import argparse
 import numpy as np
-from utils import (term_freq, idf, read_doc, extract_sentences, create_index)
 import re
-import random
-
-import datetime
 import json
+from collections import Counter
+from typing import Union, Optional, Set, Dict, List, Any, Iterable, Tuple
+
+import utils
+
 from geneeanlpclient import g3
+from geneea.sdkplus.client import AdvancedClient
+from geneea.sdkplus.request import RequestBuilder
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest='infile',  type=str,
-                        help='The input file to the script.')
-    parser.add_argument('--api-key', dest='api_key',  type=str,
-                        help='API key for the g3 client.')
-    parser.add_argument('-t', '--test', dest='test',  type=str,
-                        help='If used, then analysis is loaded and not performed.')
-    parser.add_argument("--sentences", "-s", default=True, action="store_true",
-                        help="Extract key sentences.")
-    parser.add_argument("--entity", "-e", default=False, action="store_true",
-                        help="Rows of the A matrix will represent extracted entities instead of tokens.")
-    args = parser.parse_args()
+import christiosutilities
+from sumeval.metrics.rouge import RougeCalculator
 
-    USER_KEY = args.api_key
-
-    assert not (args.infile and args.test), f"Can't input a file while in test mode. Remove the '-i' flag."
-    if args.infile:
-        assert args.api_key, f"Need an API key for the g3 client to analyze the data"
-    # Extracts sentences from the input file
-    documents, data_split_raw = [], {}
-    if args.infile:
-        with open(args.infile) as fh, g3.Client.create(userKey=USER_KEY) as analyzer:
-            requestBuilders = {
-                'high-recall': g3.Request.Builder(
-                    langDetectPrior='en',
-                    analyses=[g3.AnalysisType.ALL],
-                    referenceDate=datetime.datetime.now(),
-                    returnMentions=True,
-                    returnItemSentiment=True,
-                    domain='high-recall')
+Document = Tuple[str, Tuple[str, List[str]]]
+DocumentsRaw = Dict[str, str]
+LANGUAGE = {'english': 'en',
+            'dutch': 'nl'
             }
-            data = fh.read()
-            for domain, rb in requestBuilders.items():
-                analysis = analyzer.analyze(rb.build(text=data))
-    elif args.test:
-        with open('analysis.json', 'r') as fp:
+
+
+def prepare_data(infile: str = '',
+                 test: str = '',
+                 save: bool = False,
+                 entity: bool = False,
+                 minimum: int = 1,
+                 maximum: int = 40,
+                 lang: str = 'english'
+                 ) -> Tuple[List[Document], DocumentsRaw, g3.Analysis]:
+    """
+    - Extracts sentences from the input file
+    - documents only holds sentences which will be used in the A matrix
+    - sentences_raw[sent_id] = raw_sentence (origText)
+    """
+
+    assert infile or test, f"Either need to have an input file or be in test mode"
+    assert not (
+        infile and test), f"Can't input a file while in test mode. Remove the '-i' flag."
+
+    documents: List[Document] = []
+    sentences_raw: DocumentsRaw = {}
+    if infile:
+        client = AdvancedClient(
+            apiHost='http://alpha.g', apiPath=AdvancedClient.apiPathForWorkflow('prototype-full-analysis-news'),
+            batchSize=5, threadCount=4,
+        )
+        with open(infile) as fh:
+            rq_builder = RequestBuilder.fullAnalysis(
+                language=LANGUAGE[lang], domain='news')
+            analysis = client.analyzeOne(rq_builder.build(text=fh.read()))
+        if save:
+            with open(save, 'w') as fout:
+                print(json.dumps(g3.toDict(analysis),
+                                 ensure_ascii=False), file=fout)
+    elif test:
+        with open(test, 'r') as fp:
             analysis = g3.reader.fromDict(json.load(fp))
-    
-    token_to_entity = create_index(analysis)
+
+    token_to_entity, sent_to_token_to_rel = utils.create_index(analysis)
     data_split = [sent for sent in analysis.paragraphs[0].sentences]
     for sentence in data_split:
-        sent = read_doc(sentence, token_to_entity, entity=args.entity) # Here a document is a sentence
+        # Here a document is a sentence, and `sent` is a tuple as so: sent = (snet_id, wordlist)
+        sent = utils.read_doc(sentence, token_to_entity, sent_to_token_to_rel,
+                              entity=entity, minimum=minimum, maximum=maximum, lang=lang)
         if sent:
             documents.append(sent)
-            data_split_raw[sentence._id] = sentence.origText
+            sentences_raw[sentence._id] = sentence.origText
 
+    return documents, sentences_raw, analysis
+
+
+def prepare_A_matrix(documents, relations, cell_type='tfidf', rel=False):
     # Term Frequency
-    tf = [(sentence[0], term_freq(sentence[1])) for sentence in documents]
+    freq_list = Counter([word[0] for doc in documents for word in doc[1]])
+    tf: List[Tuple[str, Dict[str, float]]] = [(sentence[0], utils.term_freq(
+        sentence[1], cell_type, freq_list, rel=rel)) for sentence in documents]
 
     # Get the vocabulary of the documents
     vocabulary = []
@@ -74,26 +92,90 @@ if __name__ == "__main__":
         return [1.0 if (tfdict.get(word, 0) > 0) else 0. for word in vocabulary]
 
     #Create document-requency vector
-    dfvector = np.sum(np.array([docfreqmatrix(sentence[1]) for sentence in tf]), axis=0)
+    dfvector = np.sum(np.array([docfreqmatrix(sentence[1])
+                                for sentence in tf]), axis=0)
 
-    #Create term-frequency matrix
-    tf = sorted([(sentence[0], termfreqmatrix(sentence[1])) for sentence in tf], key=lambda x: x[0])
+    #Create term-frequency matrix with corresponding headers
+    tf = [(sentence[0], termfreqmatrix(sentence[1])) for sentence in tf]
     A = [sentence[1] for sentence in tf]
     columnheader = [sentence[0] for sentence in tf]
     rowheader = vocabulary
 
-    # Preparing the matrices(tfidf from tf matrix and idf vector)
+    # Preparing the matrices (tfidf from tf matrix and idf vector)
     A = np.array(A).T   # Rows are words and columns are the sentences
-    idfvector = idf(len(columnheader), dfvector)
-    idfvector = np.array(np.transpose(idfvector))
-    idfvector = np.reshape(idfvector, (-1,1))
-    A = A * idfvector
+    if cell_type and cell_type == 'tfidf':
+        idfvector = utils.idf(len(columnheader), dfvector)
+        idfvector = np.array(np.transpose(idfvector))
+        idfvector = np.reshape(idfvector, (-1, 1))
+        A = A * idfvector
 
+    return A, rowheader, columnheader
+
+
+if __name__ == "__main__":
+    count = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', dest='infile',  type=str,
+                        help='Path of the input file to the script.')
+    parser.add_argument('-r', '--reference', dest='reference',  type=str,
+                        help='Path of the reference file to compare against.')
+    parser.add_argument('-t', '--test', dest='test',  type=str,
+                        help='Path of an article\'s analysis file to use instead of the article itself. Cannot be used at the same time with -i.')
+    parser.add_argument('-eval', '--evaluation', default=False, action="store_true",
+                        help='To perform evaluation against a reference summary.')
+    parser.add_argument('-s', '--save', dest='save',  type=str,
+                        help='If an analysis on an input file was performed, saves it. To use in conjunction with -i.')
+    parser.add_argument('-n', dest='n',  type=int, default=10,
+                        help='Number of sentences used in the summary')
+    parser.add_argument('-c', '--cell-type', dest='cell_type',  type=str, default='tfidf',
+                        help='Type of values to be used for the cells of the A matrix. Current options: tf, tfidf')
+    parser.add_argument('-lang', '--language', dest='lang',  type=str, default='english',
+                        help='Language of the article (no format, just the name of the language).')
+    parser.add_argument('-l', '--length', dest='length',  type=str, default='10,40',
+                        help='Length of sentences to consider for the A matrix; format: min_length,max_length.')
+    parser.add_argument('-e', '--entity', default=False, action="store_true",
+                        help="Rows of the A matrix will represent extracted entities instead of tokens.")
+    parser.add_argument('-rel', '--relations', default=False, action="store_true",
+                        help="Relations will be included in the computation of the A matrix. Relations are note supported in Dutch")
+    parser.add_argument('-p', '--print', default=False, action="store_true",
+                        help="Print the chosen sentences to the default output stream.")
+    args = parser.parse_args()
+    min_length, max_length = tuple([int(x) for x in args.length.split(',')])
+
+    documents, sentences_raw, analysis = prepare_data(infile=args.infile,
+                                                      test=args.test,
+                                                      save=args.save,
+                                                      entity=args.entity,
+                                                      minimum=min_length,
+                                                      maximum=max_length,
+                                                      lang=args.lang)
+    A, rowheader, columnheader = prepare_A_matrix(documents=documents,
+                                                  relations=analysis.relations,
+                                                  cell_type=args.cell_type,
+                                                  rel=args.relations)
     # Singular Value Decomposition on the tfidf matrix
-    # Summary Sentences - Extraction
     U, S, VT = np.linalg.svd(A, full_matrices=0)
-    sentences = extract_sentences(VT, S, data_split_raw, columnheader)
-    # for i,concept in enumerate(concepts):
-    for i,sent in enumerate(sentences):
-        print ('[Sentence '+str(i+1)+'] :\t'+str(sent)) #Final Summary
-    print ('\n')
+    sentences = utils.extract_sentences_cross(VT=VT,
+                                              S=S,
+                                              sentences_raw=sentences_raw,
+                                              columnheader=columnheader,
+                                              n=args.n)
+    # Sort the sentences by the order in which they appear in the text
+    sentences = sorted(sentences.items(), key=lambda sent: sent[0])
+    
+    if args.print:
+        for i, (sent_id, sent) in enumerate(sentences):
+            # print('[Sentence '+str(i+1)+'] :\t'+str(sent))
+            print(str(sent))
+        print('\n')
+
+    if args.evaluation:
+        assert args.reference, f'Need a reference to compare against.'
+        with open(args.reference) as fh:
+            reference = fh.read()
+        rouge = RougeCalculator(stopwords=True, lang='en')
+        rouge_n = rouge.rouge_n(
+            summary='\n'.join([sent[1] for sent in sentences]),
+            references=reference,
+            n=1)
+        print(rouge_n)
--- text-summarization/utils.py
+++ text-summarization/utils.py
@@ -1,63 +1,173 @@
+from geneeanlpclient import g3
 import numpy as np
 from nltk.corpus import stopwords
+from nltk import edit_distance
+from operator import itemgetter
 import re
+from string import punctuation
+from typing import Union, Optional, Set, Dict, List, Any, Iterable, Tuple
+from scipy.stats import norm
+from math import exp
+
+DOUBLE_QUOTES_RE = r"[«»“”„‟❝❞〝〞〟＂]"
 
 
-def term_freq(wordlist):
+def closest_match(word, wordlist):
     """
-    Computes the term-frequency of a word list
+    Finds the closest match between a token's relations, and one of the tokens
+    in the wordlist.
     """
-    tf = dict()
-    for term in wordlist:
-        tf[term] = tf.get(term, 0.0) + 1.0
+    edit_distances = {candidate: edit_distance(
+        word, candidate) for candidate in wordlist}
+    return min(edit_distances.items(), key=itemgetter(1))[0]
+
+
+def term_freq(wordlist: List[Tuple[str, List[str]]],
+              cell_type: str,
+              freq_list=None,
+              rel=False) -> Dict[str, float]:
+    """
+    Computes the term-frequency of a word list and embeds information about
+    relations into the the term frequency list. Tokens within the same sentence
+    which are involved in a `Relation` will have 1 added to their counts to highlight
+    their relationship.
+    """
+    tf: Dict[str, float] = dict()
+    if cell_type == 'tfidf':
+        for term in wordlist:
+            tf[term[0]] = tf.get(term[0], 0.0) + 1.0
+    elif cell_type == 'tf':
+        for term in wordlist:
+            tf[term[0]] = freq_list[term[0]]
+
+    if rel:
+        for term in wordlist:
+            if term[1]:
+                tf[term[0]] += 1
+                wl = [w[0] for w in wordlist]
+                for arg in term[1]:
+                    if arg not in wl:
+                        cm = closest_match(arg, wl)
+                    else:
+                        cm = arg
+                    tf[cm] += 1
     return tf
+
 
 def idf(n, docfreq):
     """ Computes the inverse document frequency """
     return np.log10(np.reciprocal(docfreq) * n)
 
-def create_index(analysis):
-    token_to_entity = {}
+
+def create_index(analysis: g3.Analysis) -> Tuple[Dict[str, g3.Entity], Dict[str, Dict[str, List[str]]]]:
+    sent_to_token_to_rel: Dict[str, Dict[str, List[str]]] = {}
+    for rel in analysis.relations:
+        for support in rel.support:
+            args = []
+            sent_id = support.tokens[0].sentence._id
+            for arg in rel.args:
+                args.append(arg.name)
+            sent_to_token_to_rel.setdefault(sent_id, {})[
+                support.tectoToken.tokens[-1]._id] = args
+
+    token_to_entity: Dict[str, g3.Entity] = {}
     for e in analysis.entities:
         for m in e.mentions:
             for t in m.tokens:
                 token_to_entity.setdefault(t._id, []).append(e)
-    return token_to_entity
 
-def read_doc(sentence_info, token_to_entity={}, entity=False):
-    """ If entity is set to False, splits a sentence into a word list of tokens,
+    return token_to_entity, sent_to_token_to_rel
+
+
+def read_doc(sentence_info,
+             token_to_entity={},
+             sent_to_token_to_rel={},
+             entity=False,
+             minimum=1,
+             maximum=40,
+             lang='english') -> Union[None, Tuple[str, List[Tuple[str, List[str]]]]]:
+    """ If `entity` is set to False, splits a sentence into a word list of tuples (token and relation),
         else, it splits it into a word list of entites.
-        Note: Disregards sentences with less than 10 and more than 40 words (30 words
+        Note: Disregards sentences with less than `minimum` and more than `maximum` words (30 words
         when not in entity mode)."""
-    sent_id = sentence_info._id
+    sent_id: str = sentence_info._id
+    words: List[Tuple[str, List[str]]] = []
     if entity:
-        if 10 < len(sentence_info.tokens) < 40:
+        # Currently not functional, but ultimately entities should be taken into account
+        if minimum < len(sentence_info.tokens) < maximum:
             entities = []
             for t in sentence_info.tokens:
                 if t._id in token_to_entity:
                     entities.append(token_to_entity[t._id] if isinstance(
                         token_to_entity[t._id], str) else token_to_entity[t._id][0])
+            #TODO: update the type of `words` so as to match List[Tuple[str, List[str]]]
             words = [e.stdForm for e in entities]
-            return (sent_id, words)
-    else:
-        sw = stopwords.words('english')
-        if 10 < len(sentence_info.tokens) < 30:
-            words = [t.lemma for t in sentence_info.tokens if t.lemma not in sw]
-            return (sent_id, words)
 
-def extract_sentences(VT, S, data_split_raw, columnheader, k=5, n=10):
+        else:
+            return
+    else:
+        relations: Dict[str, List[str]] = sent_to_token_to_rel.get(sent_id)
+        sw = stopwords.words(lang)
+        if minimum < len(sentence_info.tokens) < maximum:
+            words: List[Tuple[str, List[str]]] = [(t.deepLemma, relations.get(t._id) if relations else None)
+                                                  for t in sentence_info.tokens if (not any(p in t.deepLemma for p in punctuation))
+                                                  ]
+        else:
+            return
+    return (sent_id, words)
+
+
+def extract_sentences_cross(VT, S, sentences_raw, columnheader, n=10) -> Dict[str, str]:
     """
-    Returns a list of n sentences (represented by the columns of VT).
-    k concepts are used.
+    - Returns a list of `n` sentences (represented by the columns of VT).
+    - `k` concepts are used if k is provided else the number of concepts is
+    the number of values in S which are >= p*max(S)
     """
-    concepts = []
-    sentences_len = np.add.reduce((np.square(VT[:k, ]).T * S[:k]).T)
-    sentences_len_sorted = sentences_len.argsort()[::-1]
-    sentences = (data_split_raw[columnheader[sentence_id]]
-                 for sentence_id in sentences_len_sorted)
-    # for _ in range(k):
-    keysentences = []
-    for _ in range(n):
-        keysentences.append(next(sentences))
-    # concepts.append(keysentences)
-    return keysentences
+    # (Ozsoy et al., 2010) set to zero all concept values (row by row) which are less than the
+    # the average concept value (for that row) for each sentence.
+    averages = np.average(VT, axis=1)
+    for i in range(VT.shape[0]):
+        for j in range(VT.shape[1]):
+            if VT[i][j] < averages[i]:
+                VT[i][j] = 0.
+    # Set `k` to be the index at which the difference between two consecutive SVs
+    # drops below the average of all differences between consecutive SVs.
+    diff_S = [x-S[i+1] for i, x in enumerate(S[:-1])]
+    avg_diff_S = sum(diff_S)/(S.size - 1)
+    for i, diff in enumerate(diff_S):
+        if diff < avg_diff_S:
+            k = i
+            break
+
+    VTk, Sk = VT[:k, ], S[:k]
+    sentences_score = np.add.reduce((np.square(VTk).T * Sk).T)
+    # Generates a function f(x) `len_weights` which sharply (polynomial of order 6)
+    # increases around x = 4 and which 'sigmoidally' decreases around 40, to give
+    # gradually less weight to longer sentences.
+    poly, sigmoid = list(range(100)), list(range(100))
+    for i, x in enumerate(poly):
+        poly[i] = -(0.06*(x-20))**6 + 1
+        sigmoid[i] = 1/(1+exp((0.4*x-20)))
+    len_weights = poly[:30] + sigmoid[32:] + [0.]*2
+
+    # Exclude sentences which are indirect speech (which contain long quoted content)
+    for sent_id, sentence in sentences_raw.items():
+        sentence = re.sub(DOUBLE_QUOTES_RE, r'"', sentence)
+        matches = re.findall(r'"(.*?)"', sentence)
+        if matches:
+            for match in matches:
+                if len(match.split(' ')) > 8:
+                    sentences_score[columnheader.index(sent_id)] = 0
+                    break
+        # For cases when segmentation is bad and we get half a quote in one sentence
+        if sentence.count(r'"') == 1:
+            sentences_score[columnheader.index(sent_id)] = 0
+        # Weigh sentences according to their length
+        sentences_score[columnheader.index(
+            sent_id)] *= len_weights[len(sentence.split(' '))]
+
+    sentences_score_sorted = sentences_score.argsort()[::-1][:n]
+    sentences: Dict[str, str] = {columnheader[sent_index]: sentences_raw[columnheader[sent_index]]
+                 for sent_index in sentences_score_sorted}
+
+    return sentences
